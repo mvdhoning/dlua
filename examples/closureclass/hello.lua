7a08fc0f3ab84822df1802b87c4caedf980fad78
mvdhoning <marten@vanderhoning.net>
2015-04-26 18:31:09 +0200

    fix lua code swap

--- examples/closureclass/hello.lua
+++ examples/closureclass/hello.lua
@@ -2,60 +2,6 @@ require "TMyClass"
 
 print("hello world, from Lua!\n")
 
-print("closure test")
-
---http://lua-users.org/wiki/ObjectOrientationTutorial
---http://www.computercraft.info/forums2/index.php?/topic/15013-setget-members-in-an-object-oriented-class/ (for properties)
---idea: generate below code from pascal with cfunction calls to pascal
---problem: get a pointer to the object in the self table (and hide that) think i use __hidden for that filled on create and passed along each call to pascal
-
--- this will be the class generated from pascal
-function MyClass(init)
-  --private
-  local self = BaseClass()
-  local _privateVariable = init --only for test
-  local function _privateVariableSetter(v) --only for test
-        _privateVariable = 2 * v --only for test
-  end
-  
-  -- add setter for each pascal property
-  local function _privateMyStringSetter(v)
-    TMyClass_Set_String(self._pascalclass,"MyString",v) --call pascal helper with pascal object property name and value
-  end
-  local function _privateMyStringGetter()
-    return TMyClass_Get_String(self._pascalclass,"MyString")
-  end
-  -- end add setter for each pascal property
-  
-  print("hello from MyClass constructor")
-  self._pascalclass = TMyClass_Create(); --call to pascal constructor
-  
-  --public 
-  
-  --functions
-  function self.foo() --lua function
-    return "hello!"
-  end
-  function self.show() --pascal function
-    return TMyClass_Show(self._pascalclass); --call with pascal object and optional parameters
-  end
-  
-  --properties
-  self._member = { key="var", --lua property
-                   set=_privateVariableSetter,
-                   get=function() return _privateVariable end } 
-  self._member = { key="MyString", --pascal property
-                   set=_privateMyStringSetter,
-                   get=_privateMyStringGetter
-                 }
-  --return the instance
-  return self
-  
-end
---end generated from pascal
-
--- start of actual lua script
-
 function MyInheritedClass(init)
   --private
   local self = MyClass(init)
@@ -67,20 +13,8 @@ function MyInheritedClass(init)
   return self
 end
 
-
---[[
-print("create myclass")
-instance = BaseClass()
-instance._member = {key="type", get=function() return "class" end}
-print(instance.type) --> class
---]]
-
 print("MyClass Tests")
-ins = MyClass(5)
-print(ins.foo()) --> Hello!
-print(ins.var) --> 5
-ins.var = 2
-print(ins.var) --> 4
+ins = MyClass()
 
 print(ins.show()) --> Show: 
 
--- examples/luascriptondemand/hello.lua
+++ examples/luascriptondemand/hello.lua
@@ -1,32 +1,5 @@
-require "TMyClass"
+require "mymodule"  
 
-print("hello world, from Lua!\n")
+print("hello world, from Lua!")  
 
-function MyInheritedClass(init)
-  --private
-  local self = MyClass(init)
-  
-  function self.Test()
-	print("Greetings from inherited class");
-  end
-  
-  return self
-end
-
-print("MyClass Tests")
-ins = MyClass()
-
-print(ins.show()) --> Show: 
-
-ins.MyString = "Hoi"
-print(ins.MyString)
-
-ins2 = MyClass(3)
-ins2.MyString = "Hallo"
-print(ins2.MyString)
-print(ins.MyString)
-
-ins3 = MyInheritedClass(5)
-ins3.Test()
-ins3.MyString ="ins3"
-print(ins3.MyString)
+foo() -- foo() is defined in mymodule 
